e533033cc3bb586c5f29738baaa22cab17f88a40
Mansour <theboom.kajas@gmail.com>
2024-09-04 12:40:38 +0200

    Ya tenemos el TableView funcionando con una celda custom

---
+++ SuperHeroes-iOS/ViewControllers/ListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  SuperHeroes-iOS
+//
+//  Created by Mañanas on 4/9/24.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var superHeroList: [SuperHero] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        
+        SuperHeroProvider.findSuperHeroesByName("Batman", withResult: { results in
+            self.superHeroList = results
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        })
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return superHeroList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SuperHeroViewCell
+        
+        let superHero = superHeroList[indexPath.row]
+        
+        cell.render(superHero: superHero)
+        
+        return cell
+    }
+    
+    
+    
+}
+
